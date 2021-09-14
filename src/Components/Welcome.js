7f737d10b423b21560e817dab39fd38a6be5e29f
webprismdevin <84688190+webprismdevin@users.noreply.github.com>
2021-09-14 14:16:20 -0700

    big doots

---
+++ src/Components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Welcome = () => {
+    return(
+        <div>
+            <h2 className="subtitle">Welcome to the Web3 Journal</h2>
+            <p className="block">The Web3 Journal is being built with complete privacy in mind. Your data is encrypted, and stored locally.</p>
+            <p className="block">You can use it offline, add it to your homescreen on your phone, or install it as an app with Chrome.</p>
+            <p className="block">You can <a href="https://github.com/webprismdevin/web3-journal">see our source code here</a></p>
+        </div>
+    )
+}
+
+export default Welcome;
